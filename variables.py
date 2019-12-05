ce5f095f9562cc868b89c050368fb4275b93fdac
Muhammed-Saleh <hamada500005@gmail.com>
2019-12-05 02:50:05 +0200

    values abstraction & sequence of instructions

---
+++ variables.py
@@ -0,0 +1,18 @@
+Python 3.0 (r30:67507, Dec  3 2008, 20:14:27) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 3.0      
+>>> pi =  3.141592
+>>> pi
+3.1415920000000002
+>>> pi_approx = 22/7
+>>> pi_approx
+3.1428571428571428
+>>> 
