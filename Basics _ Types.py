3f7159d4679a3b7b7e0e5f8a5679ebc97b12776d
Muhammed-Saleh <hamada500005@gmail.com>
2019-12-05 02:13:19 +0200

    learn types and arithmetic operators

---
+++ Basics _ Types.py
@@ -0,0 +1,38 @@
+Python 3.0 (r30:67507, Dec  3 2008, 20:14:27) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 3.0      
+>>> 3.5
+3.5
+>>> type(3)
+<class 'int'>
+>>> type(3.5)
+<class 'float'>
+>>> float(5)
+5.0
+>>> int(3.9)
+3
+>>> 3+2
+5
+>>> print(3+2)
+5
+>>> 3.0+2
+5.0
+>>> 5/2
+2.5
+>>> 5//2
+2
+>>> 5%2
+1
+>>> 3*5+1
+16
+>>> 3*(5+1)
+18
+>>> 
