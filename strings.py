18c62d4e7a93c442b489e0d51a021f8f3a74c733
Muhammed-Saleh <hamada500005@gmail.com>
2019-12-05 18:10:52 +0200

    Strings

---
+++ strings.py
@@ -0,0 +1,27 @@
+Python 3.0 (r30:67507, Dec  3 2008, 20:14:27) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 3.0      
+>>> len('hi there')
+8
+>>> 'Eric'[1]
+'r'
+>>> 'Eric'[4]
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    'Eric'[4]
+IndexError: string index out of range
+>>> 'Eric'[1:3]
+'ri'
+>>> 'Eric'[3]
+'c'
+>>> 'Eric'[:3]
+'Eri'
+>>> 
