87828e0691420f7a8f88ab52891185257b1aa2c1
Muhammed-Saleh <hamada500005@gmail.com>
2019-12-10 13:47:39 +0200

    some changes

---
+++ Calling Functions and Scope.py
@@ -0,0 +1,30 @@
+def f( x ):
+    x = x + 1
+    print('in f(x): x =', x)
+    return x
+x = 3
+z = f( x )    
+###############################
+def func_a():
+    print('inside func_a')
+def func_b(y):
+    print('inside func_b')
+    return y
+def func_c(z):
+    print('inside func_c')
+    return z()
+print(func_a())
+print(5 + func_b(2))
+print(func_c(func_a))    
+###############################
+def g(x):
+    def h():
+        x = 'abc'
+    x = x + 1
+    print('in g(x): x =', x)
+    h()
+    return x
+
+x = 3
+z = g(x)    
+
