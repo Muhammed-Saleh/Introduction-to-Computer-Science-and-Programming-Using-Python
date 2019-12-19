4b43b2040f58a4aaba20e6453742ee355a411776
Muhammed-Saleh <hamada500005@gmail.com>
2019-12-19 18:06:20 +0200

    Using Inherited Methods

---
+++ Using Inherited Methods.py
@@ -0,0 +1,98 @@
+import datetime
+
+
+class Person(object):
+    def __init__(self, name):
+        """create a person called name"""
+        self.name = name
+        self.birthday = None
+        self.lastName = name.split(" ")[-1]
+
+    def __lt__(self, other):
+        """return True if self's name is lexicographically
+        less than other's name, and False otherwise"""
+        if self.lastName == other.lastName:
+            return self.name < other.name
+        return self.lastName < other.lastName    
+
+    def setBirthday(self,month,day,year):
+        """sets self's birthday to birthDate"""
+        self.birthday = datetime.date(year,month,day)
+
+    def getAge(self):
+        """returns self's current age in days"""
+        if self.birthday == None:
+            raise ValueError
+        return (datetime.date.today() - self.birthday).days            
+
+    def getLastName(self):
+        """return self's last name"""
+        return self.lastName
+
+    def __str__(self):
+        """return self's name"""
+        return self.name
+
+class MITPerson(Person):
+    nextIdNum = 0 # next ID number to assign
+
+    def __init__(self, name):
+        Person.__init__(self, name) #initialize Person attributes
+        self.idNum = MITPerson.nextIdNum # MITPerson attribute: unique ID
+        MITPerson.nextIdNum += 1
+
+    def getIdNum(self):
+        return self.idNum
+
+    # sorting MIT people use their ID number, not name!
+    def __lt__(self, other):
+        return self.idNum < other.idNum
+
+    def speak(self, utterance):
+        return (self.name + " says: " + utterance)
+
+class Student(MITPerson):
+    pass        
+
+class UG(Student):
+    def __init__(self, name, classYear):
+        MITPerson.__init__(self, name)
+        self.year = classYear
+
+    def getClass(self):
+        return self.year
+
+    def speak(self, utterance):
+        return MITPerson.speak(self, " Ya bro, " + utterance)
+
+class Grad(Student):
+    pass
+
+class TransferStudent(Student):
+    pass
+
+
+def isStudent(obj):
+    return isinstance(obj,Student)
+
+
+# s1 = UG('Matt Damon', 2017)
+# s2 = UG('Ben Affleck', 2017)
+# s3 = UG('Lin Manuel Miranda', 2018)
+# s4 = Grad('Leonardo di Caprio')
+
+# studentList = [s1, s2, s3, s4]
+
+class Professor(MITPerson):
+    def __init__(self, name, department):
+        MITPerson.__init__(self, name)
+        self.department = department
+
+    def speak(self, utterance):
+        new = 'In course' + self.department + ' we say '
+        return MITPerson.speak(self, new + utterance)
+
+    def lecture(self, topic):
+        return self.speak('it is obvious that ' + topic)  
+
+faculty = Professor('Doctor Arrogant', 'six')      
