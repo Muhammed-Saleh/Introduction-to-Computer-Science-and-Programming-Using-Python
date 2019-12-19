3521b826822bba4d19514c5e07c0175f86b25740
Muhammed-Saleh <hamada500005@gmail.com>
2019-12-19 23:05:37 +0200

    iterations with yield

---
+++ Generators .py
@@ -0,0 +1,17 @@
+def genTest():
+    yield 1
+    yield 2
+
+foo = genTest()
+foo.__next__()    
+###############################
+def genFib():
+    fibn_1 = 1 #fib(n-1)
+    fibn_2 = 0 #fib(n-2)
+    while True:
+        #fib(n) = fib(n-1) + fib(n-2)
+        next = fibn_1 + fibn_2
+        yield next
+        fibn_2 = fibn_1
+        fibn_1 = next
+
